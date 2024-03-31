d95adfe09064ddf281ead83dc522e85a5edfdd27
Sall <sallaboudaouda@gmail.com>
2024-03-31 18:03:12 +0000

    fix get all route

--- src/app/api/get-all-products/route.ts
+++ src/app/api/get-all-products/route.ts
@@ -1,19 +1,16 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../libs/prisma.db";
+import { NextApiResponse } from "next";
 
-export async function GET(res: NextResponse) {
+export async function GET(req: Request, res: NextApiResponse) {
   try {
     const products = await prisma.products.findMany();
 
-    return NextResponse.json(
-      {
-        message: "success",
-        products,
-      },
-      { status: 200 },
-    );
+    return res.status(200).json({
+      message: "success",
+      products,
+    });
   } catch (error) {
-    console.log(error);
     return NextResponse.json({ error });
   }
 }
