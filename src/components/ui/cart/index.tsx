152cd74d9e0eaf78bd0ab65b250a3eb15313ea4e
Sall <sallaboudaouda@gmail.com>
2024-06-24 19:41:51 +0000

    fix: fix cart reducer and ui

--- src/components/ui/cart/index.tsx
+++ src/components/ui/cart/index.tsx
@@ -3,50 +3,62 @@ import Link from "next/link";
 
 import { X } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { useSelector } from "react-redux";
-import { selectCart, selectCartItems, selectCartSubtotal } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart, selectCartItems, selectCartSubtotal } from "@/store";
+import { truncateTitle } from "@/utils";
+
+import "./style.css";
 
 interface CartProps {
   setIsOpen: (isOpen: boolean) => void;
 }
 
 export const Cart = ({ setIsOpen }: CartProps) => {
+  const dispatch = useDispatch();
   const t = useTranslations("Cart");
-  const cart = useSelector(selectCart);
   const items = useSelector(selectCartItems);
   const subTotal = useSelector(selectCartSubtotal);
 
-  console.log(items, "items");
+  const handleRemove = (id: string) => {
+    const message = t("itemRemovedFromCart");
+    dispatch(removeFromCart({ id, message }));
+  };
 
   return (
     <div className="h-[90%]">
       {items.length > 0 ? (
         <div className="mt-4 flex h-full flex-col justify-between">
-          {items.map(({ id, amount, promoPrice, defaultImage, title }) => (
-            <div className="flex items-center justify-between gap-5" key={id}>
-              <div
-                style={{
-                  backgroundImage: `url(${defaultImage})`,
-                  backgroundSize: "contain",
-                }}
-                className="h-20 w-20"
-              ></div>
-              <div>
-                <h2 className="font-semibold">{title}</h2>
-                <p className="mt-2">
-                  <span className="mr-2 text-sm">{amount}</span> x{" "}
-                  <span className="ml-2 text-sm text-orange-500">
-                    {promoPrice}
-                  </span>
-                </p>
+          <div className="car-overflow-bar mt-4 flex h-full flex-col gap-8 overflow-y-auto">
+            {items.map(({ id, amount, promoPrice, defaultImage, title }) => (
+              <div className="flex items-center justify-between gap-5" key={id}>
+                <div
+                  style={{
+                    backgroundImage: `url(${defaultImage})`,
+                    backgroundSize: "contain",
+                  }}
+                  className="h-20 w-20"
+                ></div>
+                <div>
+                  <h2 className="text-xs font-semibold">
+                    {truncateTitle(title, 24)}
+                  </h2>
+                  <p className="mt-2">
+                    <span className="mr-2 text-xs">{amount}</span> x{" "}
+                    <span className="ml-2 text-xs text-orange-500">
+                      {promoPrice}
+                    </span>
+                  </p>
+                </div>
+                <div
+                  className="flex h-6 w-6 items-center justify-center rounded-full hover:bg-gray-200"
+                  onClick={() => handleRemove(id)}
+                >
+                  <X size={18} className="hover:text-white-100" />
+                </div>
               </div>
-              <div className="flex h-6 w-6 items-center justify-center rounded-full hover:bg-gray-200">
-                <X size={18} className="hover:text-white-100" />
-              </div>
-            </div>
-          ))}
-
-          <div>
+            ))}
+          </div>
+          <div className="mt-4">
             <div className="flex items-center justify-between border-b border-b-gray-300 pb-4">
               <h2 className="font-medium">{t("subTotal")}</h2>
               <p>
