235a4ce8e2703f1f0da36502d992c6be891b70f2
Sall <sallaboudaouda@gmail.com>
2024-08-30 18:38:25 +0000

    fix products folder structure

--- src/actions/products.actions.ts
+++ src/actions/products.actions.ts
@@ -1,26 +1,26 @@
 import { instance } from "@/config";
 
 export const getAllProducts = async (count: number) => {
-  const { data } = await instance.get(`/get-all-products?count=${count}`);
+  const { data } = await instance.get(`/products/all-products?count=${count}`);
 
   return data;
 };
 
 export const getProduct = async (id: string) => {
-  const { data } = await instance.get(`/get-product?id=${id}`);
+  const { data } = await instance.get(`/products/product?id=${id}`);
 
   return data;
 };
 
 export const getSearch = async (category: string) => {
-  const { data } = await instance.get(`/search?category=${category}`);
+  const { data } = await instance.get(`/products/search?category=${category}`);
 
   return data;
 };
 
 export const getRelatedProducts = async (count: number, category: string) => {
   const { data } = await instance.get(
-    `/get-related-products?count=${count}&category=${category}`,
+    `/products/related?count=${count}&category=${category}`,
   );
 
   return data;
--- src/app/api/products/product/route.ts
+++ src/app/api/products/product/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../libs/prisma.db";
+import prisma from "../../../../../libs/prisma.db";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   const id = req.nextUrl.searchParams.get("id");
