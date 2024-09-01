e18d11f651dd2112d235843ef10198f3d4fff1c5
Sall <sallaboudaouda@gmail.com>
2024-09-01 12:09:02 +0000

    added orders messages

--- src/app/[locales]/orders/page.tsx
+++ src/app/[locales]/orders/page.tsx
@@ -6,8 +6,10 @@ import { getOrders } from "@/actions";
 import { Layout, OrdersCard } from "@/components";
 import { OrderWithItems } from "@/types";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 const Orders = () => {
+  const t = useTranslations("Orders");
   const { error, data, isLoading } = useSWR(["Orders"], getOrders, {
     revalidateOnFocus: false,
   });
@@ -18,18 +20,15 @@ const Orders = () => {
 
   return (
     <Layout className="px-5 pb-20 pt-10 lg:pb-40">
-      <h2 className="mb-8 text-xl font-semibold">My orders</h2>
+      <h2 className="mb-8 text-xl font-semibold">{t("Orders")}</h2>
       <div className="flex flex-col gap-8">
-        {isLoading && <p>Loading orders...</p>}
-        {error && <p>Error loading orders.</p>}
+        {isLoading && <p>{t("loading")}</p>}
+        {error && <p>{t("errorLoading")}</p>}
         {data?.orders && data.orders.length === 0 && (
           <p className="flex flex-col text-center text-gray-500">
-            <span>
-              You have no orders yet. Explore our products and place your first
-              order!
-            </span>
+            <span>{t("noOrders")}</span>
             <Link href="/" className="text-blue-500 underline">
-              Go to Home
+              {t("goHome")}
             </Link>
           </p>
         )}
