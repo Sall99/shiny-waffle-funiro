0884016e353e709b0216c372dd1a776598fa7f3a
Sall <sallaboudaouda@gmail.com>
2024-09-10 17:07:26 +0000

    added some seo stuff

--- src/app/[locales]/page.tsx
+++ src/app/[locales]/page.tsx
@@ -7,6 +7,16 @@ import {
   Gallery,
 } from "@/components";
 
+const GallerySection = () => {
+  return (
+    <div className="mb-8 text-center">
+      <p className="text-xs font-semibold">Share your setup with</p>
+      <h2 className="text-lg font-bold">Gallery</h2>
+      <Gallery />
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -16,11 +26,7 @@ export default function Home() {
         <ProductsSection />
         <Inspiration />
       </Layout>
-      <div className="mb-8 text-center">
-        <p className="text-xs font-semibold">Share your setup with</p>
-        <h2 className="text-lg font-bold">Gallery</h2>
-        <Gallery />
-      </div>
+      <GallerySection />
     </>
   );
 }
